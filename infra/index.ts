595852fb031efc83ceabcbfa561dfb3916d386b3
ethan-stone <ethan.stone9352@gmail.com>
2022-05-29 23:16:12 -0400

    reorganized server and created client app

--- infra/index.ts
+++ infra/index.ts
@@ -1,72 +1,72 @@
-import * as pulumi from '@pulumi/pulumi';
-import * as gcp from '@pulumi/gcp';
-import * as docker from '@pulumi/docker';
-import * as fs from 'fs';
+import * as pulumi from "@pulumi/pulumi";
+import * as gcp from "@pulumi/gcp";
+import * as docker from "@pulumi/docker";
+import * as fs from "fs";
 
-const imageName = 'api-app';
+const imageName = "api-app";
 
 const apiImage = new docker.Image(imageName, {
   imageName: pulumi.interpolate`gcr.io/${gcp.config.project}/${imageName}`,
   build: {
-    context: '../',
-  },
+    context: "../server"
+  }
 });
 
-const apiService = new gcp.cloudrun.Service('api-service', {
-  location: 'us-central1',
+const apiService = new gcp.cloudrun.Service("api-service", {
+  location: "us-central1",
   template: {
     spec: {
       containers: [
         {
-          image: apiImage.imageName,
-        },
-      ],
+          image: apiImage.imageName
+        }
+      ]
     },
     metadata: {
       annotations: {
-        'autoscaling.knative.dev/minScale': '0',
-        'autoscaling.knative.dev/maxScale': '1',
-      },
-    },
-  },
+        "autoscaling.knative.dev/minScale": "0",
+        "autoscaling.knative.dev/maxScale": "1"
+      }
+    }
+  }
 });
 
-new gcp.cloudrun.IamMember('api-iam', {
+new gcp.cloudrun.IamMember("api-iam", {
   service: apiService.name,
-  location: 'us-central1',
-  role: 'roles/run.invoker',
-  member: 'allUsers',
+  location: "us-central1",
+  role: "roles/run.invoker",
+  member: "allUsers"
 });
 
 export const apiServiceUrl = apiService.statuses[0].url;
 
-const api = new gcp.apigateway.Api('api', { apiId: 'api' });
+const api = new gcp.apigateway.Api("api", { apiId: "api" });
 
-const apiConfigStr = fs.readFileSync('../openapi-spec.yaml').toString();
+const apiConfigStr = fs.readFileSync("../server/openapi-spec.yaml").toString();
 
 const newApiConfigStr = apiServiceUrl.apply((apiServiceUrlValue) =>
   Buffer.from(
-    apiConfigStr.replace('${api_backend}', apiServiceUrlValue),
-  ).toString('base64'),
+    apiConfigStr.replace("${api_backend}", apiServiceUrlValue)
+  ).toString("base64")
 );
 
-const apiConfig = new gcp.apigateway.ApiConfig('apiConfig', {
+const apiConfig = new gcp.apigateway.ApiConfig("apiConfig", {
   api: api.apiId,
-  apiConfigIdPrefix: 'config',
+  apiConfigIdPrefix: "config",
   openapiDocuments: [
     {
       document: {
-        path: 'openapi-spec.yaml',
-        contents: newApiConfigStr,
-      },
-    },
-  ],
+        path: "openapi-spec.yaml",
+        contents: newApiConfigStr
+      }
+    }
+  ]
 });
 
-const apiGateway = new gcp.apigateway.Gateway('apiGateway', {
-  region: 'us-central1',
+const apiGateway = new gcp.apigateway.Gateway("apiGateway", {
+  region: "us-central1",
   apiConfig: apiConfig.id,
-  gatewayId: 'api-gw',
+  gatewayId: "api-gw"
 });
 
 export const apiGatewayUrl = apiGateway.defaultHostname;
--- server/src/auth/types/index.ts
+++ server/src/auth/types/index.ts
@@ -1,4 +1,4 @@
 export * from './at.type';
 export * from './rt.type';
 export * from './tokens.type';
-export * from './/signup-args.type';
+export * from './signup-args.type';
